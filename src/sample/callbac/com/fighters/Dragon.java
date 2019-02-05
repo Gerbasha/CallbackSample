ce54b9e2c7f8e70a5db75dd9ff6a7f586c637035
Ruslan <mckruslan@gmail.com>
2019-02-05 14:08:41 +0200

    Added functionality to classes: BaseFighter (and made it abstract); Dragon; DragonRider; ElementPower;

--- src/sample/callbac/com/fighters/Dragon.java
+++ src/sample/callbac/com/fighters/Dragon.java
@@ -1,13 +1,46 @@
-package sample.callbac.com.fighters;
+package src.sample.callbac.com.fighters;
 
-import javax.swing.text.html.parser.Element;
+import src.sample.callbac.com.fighters.base.BaseFighter;
+import src.sample.callbac.com.fighters.base.PreFightActions;
 
-public class Dragon implements ElementPower {
+public class Dragon extends BaseFighter implements ElementPower, PreFightActions {
 
     int elementMask;
+    Elements[] elements;
+
+    public Dragon(String name, int health, int attack, double defense, Elements[] elements) {
+        super(name, health, attack, defense);
+        this.elements = elements;
+    }
 
     @Override
     public int getElementPower() {
-        return elementMask;
+        int byteMask = 0;
+        for (Elements i : elements){
+            byteMask |= i.getValue();
+        }
+        return byteMask;
+    }
+
+    public int getPowerMultiplier(int power) {
+        int difPower = getElementPower() ^ power;
+        int tempDifPower = ~difPower;
+        int reverseElements = tempDifPower | power;
+        int elements = ~ reverseElements;
+        int count = 1;
+        for (Elements p : Elements.values()) {
+            count += (elements & p.getValue()) > 0 ? 1 : 0;
+        }
+        return count;
+    }
+
+    public void countAttack(BaseFighter fighter) {
+        int bonusAttack = getPowerMultiplier(fighter instanceof Dragon ? ((Dragon) fighter).getElementPower() : 0);
+        setAttack(getAttack() * bonusAttack);
+    }
+
+    @Override
+    public void actionWithFight(BaseFighter fighter) {
+        countAttack(fighter);
     }
 }
--- src/sample/callbac/com/fighters/DragonRider.java
+++ src/sample/callbac/com/fighters/DragonRider.java
@@ -1,9 +1,48 @@
-package sample.callbac.com.fighters;
+package src.sample.callbac.com.fighters;
 
-import sample.callbac.com.fighters.base.BaseFighter;
+import src.sample.callbac.com.fighters.base.BaseFighter;
+import src.sample.callbac.com.fighters.base.FightBreakAction;
+import src.sample.callbac.com.fighters.base.PostFightActions;
 
-public class DragonRider extends BaseFighter {
+public class DragonRider extends BaseFighter implements PostFightActions, FightBreakAction {
 
     Dragon dragonPet = null;
+    private int pureAttack;
 
+    public DragonRider(String name, int health, int attack, double defense) {
+        super(name, health, attack, defense);
+        pureAttack = attack;
+    }
+    public void countHealth() {
+        setHealth(getHealth() + dragonPet.getHealth());
+    }
+
+    public void countAttack() {
+        setAttack(getAttack() + dragonPet.getAttack());
+    }
+
+    private boolean isDragonAlive() {
+        return getHealth() > getFullHP();
+    }
+
+    @Override
+    public void action(BaseFighter opponent, int takenDamage) {
+        if (!isDragonAlive()) {
+            opponent.setAttack(getPureAttack());
+        }
+    }
+
+    public int getPureAttack() {
+        return pureAttack;
+    }
+
+    @Override
+    public void actionWithFight(BaseFighter fighter, FightCallBack callBack) {
+        if (!isDragonAlive()){
+            dragonPet = (Dragon) fighter;
+            countAttack();
+            countHealth();
+            callBack.fightImidiatlyDone(this);
+        }
+    }
 }
